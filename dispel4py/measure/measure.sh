de9ec4f088c0dcbd6944c2feb61c244170491c68
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2017-07-10 00:49:33 +0100

    update measure.sh

--- dispel4py/measure/measure.sh
+++ dispel4py/measure/measure.sh
@@ -8,7 +8,9 @@ measure_dir=$PWD/measure
 mkdir -p "$measure_dir"
 
 overall_file="$measure_dir/overall"
-touch "$overall_file"
+if [ ! -f "$overall_file" ]; then
+	echo platform number_of_iteration np max_number_of_sieves max_prime mpi_time mpi_inc_time
+fi
 
 source ~/self/Edinburgh/venv/dissertation/bin/activate
 
@@ -18,6 +20,8 @@ function step {
 
 	max_number_of_sieves=$3
 	max_prime=$4
+
+	echo np:$np number_of_iteration:$number_of_iteration max_number_of_sieves:$max_number_of_sieves max_prime:$max_prime
 
 	wd=$measure_dir/`date` &&
 	mkdir -p "$wd" &&
@@ -36,22 +40,28 @@ function step {
 	mpi_inc_time=`cat time_mpi_inc | tr -d '\n'` &&
 	mpi_time=`cat time_mpi | tr -d '\n'` &&
 
+	echo $platform $number_of_iteration $np $max_number_of_sieves $max_prime $mpi_time $mpi_inc_time
 	echo $platform $number_of_iteration $np $max_number_of_sieves $max_prime $mpi_time $mpi_inc_time >> "$overall_file"
 }
 
 pair_1="26 100"
 pair_2="168 1000"
 pair_3="1397 10000"
-all=(pair_1 pair_2 pair_3)
+all=(
+pair_1
+pair_2
+pair_3
+)
 
 np=3
 
+echo Ready
+
 length=${#all[@]}
 for ((i=0;i<$length;i++)) do
-	max_sieve=${!all[i]:0}
-	max_prime=${!all[i]:1}
+	pair=${!all[i]}
 	for ((number_of_iteration=1;number_of_iteration<100;number_of_iteration+=10)); do
-		for ((i=0;i<8;i+=1)); do step $np $number_of_iteration $max_sieve $max_prime; done
+		for ((i=0;i<8;i+=1)); do step $np $number_of_iteration $pair; done
 	done
 done
 
