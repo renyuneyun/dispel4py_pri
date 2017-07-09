d67575311b4a45acdf53862ec18999823b0d032a
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2017-07-10 00:12:21 +0100

    forgot to add graph

--- dispel4py/measure/measure.sh
+++ dispel4py/measure/measure.sh
@@ -27,9 +27,9 @@ function step {
 	echo Number of initial nodes: $np > configure &&
 	echo Number of iterations: $number_of_iteration >> configure &&
 
-	/usr/bin/time -f %e mpiexec -np $np dispel4py mpi_inc dispel4py.examples.graph_testing.measure.repeatable_prime_sieve -i $number_of_iteration > /dev/null 2> time_mpi_inc &&
+	/usr/bin/time -f %e mpiexec -np $np dispel4py mpi_inc dispel4py.measure.graph.repeatable_prime_sieve -i $number_of_iteration > /dev/null 2> time_mpi_inc &&
 	echo "=================" &&
-	/usr/bin/time -f %e mpiexec -np $(($max_number_of_sieves+1)) dispel4py mpi dispel4py.examples.graph_testing.measure.repeatable_prime_sieve__static_$max_number_of_sieves -i $number_of_iteration > /dev/null 2> time_mpi &&
+	/usr/bin/time -f %e mpiexec -np $(($max_number_of_sieves+1)) dispel4py mpi dispel4py.measure.graph.repeatable_prime_sieve__static_$max_number_of_sieves -i $number_of_iteration > /dev/null 2> time_mpi &&
 
 	#mpi_inc_time=`cat mpi_inc | tr -d '\n'` &&
 	#mpi_time=`cat mpi | tr -d '\n'` &&
@@ -39,17 +39,17 @@ function step {
 	echo $platform $number_of_iteration $np $max_number_of_sieves $max_prime $mpi_time $mpi_inc_time >> "$overall_file"
 }
 
-pair_1=(26 100)
-pair_2=(168 1000)
-pair_3=(1397 10000)
-all=(pair_1[@] pair_2[@] pair_3[@])
+pair_1="26 100"
+pair_2="168 1000"
+pair_3="1397 10000"
+all=(pair_1 pair_2 pair_3)
 
 np=3
 
 length=${#all[@]}
 for ((i=0;i<$length;i++)) do
-	max_sieve=${!all[i]:0:1}
-	max_prime=${!all[i]:1:1}
+	max_sieve=${!all[i]:0}
+	max_prime=${!all[i]:1}
 	for ((number_of_iteration=1;number_of_iteration<100;number_of_iteration+=10)); do
 		for ((i=0;i<8;i+=1)); do step $np $number_of_iteration $max_sieve $max_prime; done
 	done
