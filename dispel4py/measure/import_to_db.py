0fd626faf7f4195190792a614c0907fce0e9a547
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2017-07-17 22:38:35 +0100

    use ignore/outlier mark in csv file

--- dispel4py/measure/import_to_db.py
+++ dispel4py/measure/import_to_db.py
@@ -21,8 +21,10 @@ def import_every_line(lines):
     for i, line in enumerate(lines):
         if i == 0 and line.startswith('platform '):
             continue
+        is_outlier = False
         if line.startswith('#'):
-            continue
+            is_outlier = True
+            line = line[1:]
         parts = line.split()
         platform = ''
         varsion = ''
@@ -38,8 +40,8 @@ def import_every_line(lines):
             except ValueError:
                 platform, version, run_id, num_of_iter, np, num_of_sieve, max_prime, mpi_time, mpi_inc_time = parts
         num_of_iter, np, num_of_sieve, max_prime, mpi_time, mpi_inc_time = int(num_of_iter), int(np), int(num_of_sieve), int(max_prime), float(mpi_time), float(mpi_inc_time)
-        ins1 = record.insert().values(platform=platform, version=version, run_id=run_id, module='mpi', num_iter=num_of_iter, np=np, max_num_sieve=num_of_sieve, max_prime=max_prime, time=mpi_time)
-        ins2 = record.insert().values(platform=platform, version=version, run_id=run_id, module='mpi_inc', num_iter=num_of_iter, np=np, max_num_sieve=num_of_sieve, max_prime=max_prime, time=mpi_inc_time)
+        ins1 = record.insert().values(outlier=is_outlier, platform=platform, version=version, run_id=run_id, module='mpi', num_iter=num_of_iter, np=np, max_num_sieve=num_of_sieve, max_prime=max_prime, time=mpi_time)
+        ins2 = record.insert().values(outlier=is_outlier, platform=platform, version=version, run_id=run_id, module='mpi_inc', num_iter=num_of_iter, np=np, max_num_sieve=num_of_sieve, max_prime=max_prime, time=mpi_inc_time)
         try:
             result = conn.execute(ins1)
         except IntegrityError:
