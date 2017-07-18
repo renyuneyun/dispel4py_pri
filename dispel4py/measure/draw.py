46ebd219f83f3f9466d516744aa617ebf1335031
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2017-07-18 01:46:32 +0100

    make query more flexible && combine old

--- dispel4py/measure/draw.py
+++ dispel4py/measure/draw.py
@@ -21,86 +21,60 @@ conn = engine.connect()
 
 combine_different_runs = True
 
-def all_platforms():
-    s = select([record.c.platform, record.c.version, record.c.run_id]).distinct()
+def R(name):
+    return getattr(record.c, name)
+
+def R_(names):
+    return list(map(R, names))
+
+def pure(pairs, order=[]):
+    if order:
+        return [pairs[name] for name in order]
+    else:
+        return pairs.values()
+
+def all_of(columns, where={}, distinct=False, order_by=[]):
+    targets = R_(columns)
+    s = select(targets)
+    for k, v in where.items():
+        s = s.where(R(k) == v)
+    if distinct:
+        s = s.distinct()
+    if order_by:
+        s = s.order_by(*R_(order_by))
     results = conn.execute(s)
     for line in results:
-        yield line[0]
+        yield dict(zip(columns, line))
 
-def all_confs_of_platform(platform):
-    targets = [record.c.max_num_sieve, record.c.max_prime]
-    s = select(targets) \
-            .where(record.c.platform==platform[0]).where(record.c.version==platform[1]).where(record.c.run_id==platform[2]) \
-            .distinct()
-    results = conn.execute(s)
-    for line in results:
-        yield line
+def get_times(where):
+    targets = ['num_iter', 'np', 'time']
+    order_by = ['np', 'num_iter']
+    times = {}
+    for line in all_of(targets, where, order_by=order_by):
+        num_iter = line['num_iter']
+        num_p = line['np']
+        time = line['time']
+        if num_p not in times:
+            times[num_p] = ([], [])
+        con = times[num_p]
+        if con[0] and con[0][-1] == num_iter:
+            con[1][-1].append(time)
+        else:
+            con[0].append(num_iter)
+            con[1].append([time])
+    return times
 
-def all_confs():
-    targets = [record.c.max_num_sieve, record.c.max_prime]
-    s = select(targets).distinct().order_by(record.c.max_num_sieve)
-    results = conn.execute(s)
-    for line in results:
-        yield line
+def get_mpi(platform, conf):
+    where = {**platform, **conf}
+    where['outlier'] = False
+    where['module'] = 'mpi'
+    return get_times(where)
 
-def all_platforms_of_conf(conf):
-    targets = [record.c.platform, record.c.version]
-    if not combine_different_runs:
-        targets.append(record.c.run_id)
-    s = select(targets).where(record.c.max_num_sieve==conf[0]).where(record.c.max_prime==conf[1]).distinct()
-    results = conn.execute(s)
-    for line in results:
-        yield line
-
-def all_time_of_conf(platform, conf):
-    def get_mpi():
-        mpi_times = {}
-        s = select([record.c.num_iter, record.c.np, record.c.time]) \
-                .where(record.c.outlier==False).where(record.c.module=='mpi') \
-                .where(record.c.platform==platform[0])
-        if not combine_different_runs:
-            s = s.where(record.c.run_id==platform[2])
-        s = s.where(record.c.max_num_sieve==conf[0]).where(record.c.max_prime==conf[1]) \
-                .order_by(record.c.np, record.c.num_iter)
-        results = conn.execute(s)
-        for line in results:
-            num_iter = line[0]
-            num_p = line[1]
-            time = line[2]
-            if num_p not in mpi_times:
-                mpi_times[num_p] = ([], [])
-            con = mpi_times[num_p]
-            if con[0] and con[0][-1] == num_iter:
-                con[1][-1].append(time)
-            else:
-                con[0].append(num_iter)
-                con[1].append([time])
-        return mpi_times
-    def get_mpi_inc():
-        mpi_inc_times = {}
-        s = select([record.c.num_iter, record.c.np, record.c.time]) \
-                .where(record.c.outlier==False).where(record.c.module=='mpi_inc') \
-                .where(record.c.platform==platform[0]).where(record.c.version==platform[1])
-        if not combine_different_runs:
-            s = s.where(record.c.run_id==platform[2])
-        s = s.where(record.c.max_num_sieve==conf[0]).where(record.c.max_prime==conf[1]) \
-                .order_by(record.c.np, record.c.num_iter)
-        results = conn.execute(s)
-        for line in results:
-            num_iter = line[0]
-            num_p = line[1]
-            time = line[2]
-            if num_p not in mpi_inc_times:
-                mpi_inc_times[num_p] = ([], [])
-            con = mpi_inc_times[num_p]
-            if con[0] and con[0][-1] == num_iter:
-                con[1][-1].append(time)
-            else:
-                con[0].append(num_iter)
-                con[1].append([time])
-        return mpi_inc_times
-    mpi_times, mpi_inc_times = get_mpi(), get_mpi_inc()
-    return mpi_times, mpi_inc_times
+def get_mpi_inc(platform, conf):
+    where = {**platform, **conf}
+    where['outlier'] = False
+    where['module'] = 'mpi_inc'
+    return get_times(where)
 
 avg = lambda lst: sum(lst) / len(lst)
 flatten = lambda l: [item for sub in l for item in sub]
@@ -108,30 +82,55 @@ expand = lambda l1, l2: flatten([[item] * len(l2[i]) for i, item in enumerate(l1
 
 capsize = 5
 
-confs = list(all_confs())
-fig, axes = plt.subplots(len(confs), sharex=True)
+conf_cols = ['max_num_sieve', 'max_prime']
+
+platform_columns_mpi = ['platform'] + ([] if combine_different_runs else ['run_id'])
+
+def key_mpi_platform(platform):
+    return tuple(platform[name] for name in platform_columns_mpi)
+
+mpi_times = {tuple(pure(conf, conf_cols)):
+        {key_mpi_platform(platform):
+            get_mpi(platform, conf)
+            for platform in all_of(platform_columns_mpi, where=conf, distinct=True)}
+        for conf in all_of(conf_cols, where={'module':'mpi'}, distinct=True, order_by=['max_num_sieve'])}
+
+confs_mpi_inc = list(all_of(conf_cols, where={'module':'mpi_inc'}, distinct=True, order_by=['max_num_sieve']))
+fig, axes = plt.subplots(len(confs_mpi_inc), sharex=True)
 
 fig.text(0.5, 0.04, 'number of iterations', ha='center')
 fig.text(0.04, 0.5, 'time', va='center', rotation='vertical')
 
-for i, conf in enumerate(confs):
+drawn = {}
+for i, conf in enumerate(confs_mpi_inc):
     subplot = axes[i]
-    subplot.set_title("max_num_sieve:{} max_prime:{}".format(conf[0], conf[1]))
-    for platform in all_platforms_of_conf(conf):
-        platform_str = str(platform[0])
-        if platform[1]:
-            platform_str += "-{}".format(platform[1])
-            if not combine_different_runs and platform[2]:
-                platform_str += "-{}".format(platform[2])
+    subplot.set_title("max_num_sieve:{} max_prime:{}".format(conf['max_num_sieve'], conf['max_prime']))
+    k_conf = tuple(pure(conf, conf_cols))
+    drawn[k_conf] = {}
+    for platform in all_of(['platform', 'version'] + ([] if combine_different_runs else ['run_id']), where=conf, distinct=True):
+        k_platform = key_mpi_platform(platform)
+        if k_platform not in drawn[k_conf]:
+            try:
+                platform_str = str(platform['platform'])
+                if not combine_different_runs and platform['run_id']:
+                    platform_str += "-{}".format(platform['run_id'])
+                for num_p, (num_iters, times) in mpi_times[k_conf][k_platform].items():
+                    label_old = "old {} np:{}".format(platform_str, num_p)
+                    p = subplot.errorbar(num_iters, list(map(avg, times)), yerr=list(map(np.std, times)), capsize=capsize, linestyle='dashed', label=label_old)
+                    color = p[0].get_color()
+                    subplot.plot(list(expand(num_iters, times)), list(flatten(times)), '.', color=color)
+                drawn[k_conf][k_platform] = True
+            except KeyError:
+                drawn[k_conf][k_platform] = False
+        platform_str = str(platform['platform'])
+        if platform['version']:
+            platform_str += "-{}".format(platform['version'])
+            if not combine_different_runs and platform['run_id']:
+                platform_str += "-{}".format(platform['run_id'])
         else:
-            if not combine_different_runs and platform[2]:
-                platform_str += "--{}".format(platform[2])
-        mpi_times, mpi_inc_times = all_time_of_conf(platform, conf)
-        for num_p, (num_iters, times) in mpi_times.items():
-            label_old = "old {} np:{}".format(platform_str, num_p)
-            p = subplot.errorbar(num_iters, list(map(avg, times)), yerr=list(map(np.std, times)), capsize=capsize, linestyle='dashed', label=label_old)
-            color = p[0].get_color()
-            subplot.plot(list(expand(num_iters, times)), list(flatten(times)), '.', color=color)
+            if not combine_different_runs and platform['run_id']:
+                platform_str += "--{}".format(platform['run_id'])
+        mpi_inc_times = get_mpi_inc(platform, conf)
         for num_p, (num_iters, times) in mpi_inc_times.items():
             label_mine = "mine {} np:{}".format(platform_str, num_p)
             p = subplot.errorbar(num_iters, list(map(avg, times)), yerr=list(map(np.std, times)), capsize=capsize, label=label_mine)
