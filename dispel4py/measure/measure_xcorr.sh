d4de6f654d705182a22757034111951bd9478fdb
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2017-08-13 17:54:07 +0100

    update records in measure_xcorr.sh

--- dispel4py/measure/measure_xcorr.sh
+++ dispel4py/measure/measure_xcorr.sh
@@ -11,19 +11,20 @@ version=opt1.1_
 run_id=`date +%Y-%m-%d.%H:%M:%S`
 
 source ~/self/Edinburgh/venv/dissertation/bin/activate
-xcorr_dir="$HOME/self/Edinburgh/MSc Dissertation/dispel4py_workflows/tc_cross_correlation"
-export PYTHONPATH=$PYTHONPATH:"$xcorr_dir"
+xcorr_base_dir="$HOME/self/Edinburgh/MSc Dissertation/dispel4py_workflows"
+export PYTHONPATH=$PYTHONPATH:"$xcorr_base_dir"
+xcorr_dir="$xcorr_base_dir/tc_cross_correlation"
+
+days=90day
+trs=2tr
 
 measure_dir=$PWD/measure_xcorr
 mkdir -p "$measure_dir"
 
 overall_file="$measure_dir/overall"
 if [ ! -f "$overall_file" ]; then
-	echo workflow platform version run_id module number_of_iteration np max_number_of_sieves max_prime time | tee "$overall_file"
+	echo workflow platform version run_id module days traces np time | tee "$overall_file"
 fi
-
-export DISPEL4PY_XCORR_STARTTIME=2015-04-06T06:00:00.000
-export DISPEL4PY_XCORR_ENDTIME=2015-04-06T07:00:00.000
 
 function step {
 	np=$1
@@ -69,35 +70,34 @@ function step {
 	mkdir -p $xcorr_gen_dir &&
 
 	echo Number of initial nodes: $np > configure &&
-	echo Number of iterations: $number_of_iteration >> configure &&
 
 	rm -rf "$xcorr_gen_dir/DATA" &&
 	mkdir "$xcorr_gen_dir/DATA" &&
 	echo $exec_mpi_inc_prep &&
 	eval $exec_mpi_inc_prep > stdout_mpi_inc_prep 2> stderr_mpi_inc_prep &&
 	mpi_inc_time_prep=`cat $fn_t_mpi_inc_prep | tr -d '\n'` &&
-	echo $wf_mpi_inc_prep $platform $version $run_id mpi_inc $number_of_iteration $np_mpi_inc_prep $max_number_of_sieves $max_prime $mpi_inc_time_prep | tee -a "$overall_file" &&
+	echo $wf_mpi_inc_prep $platform $version $run_id mpi_inc $days $trs $np_mpi_inc_prep $mpi_inc_time_prep | tee -a "$overall_file" &&
 
 	rm -rf "$xcorr_gen_dir/XCORR" &&
 	mkdir "$xcorr_gen_dir/XCORR" &&
 	echo $exec_mpi_inc_xcorr &&
 	eval $exec_mpi_inc_xcorr > stdout_mpi_inc_xcorr 2> stderr_mpi_inc_xcorr &&
 	mpi_inc_time_xcorr=`cat $fn_t_mpi_inc_xcorr | tr -d '\n'` &&
-	echo $wf_mpi_inc_xcorr $platform $version $run_id mpi_inc $number_of_iteration $np_mpi_inc_xcorr $max_number_of_sieves $max_prime $mpi_inc_time_xcorr | tee -a "$overall_file"
+	echo $wf_mpi_inc_xcorr $platform $version $run_id mpi_inc $days $trs $np_mpi_inc_xcorr $mpi_inc_time_xcorr | tee -a "$overall_file"
 
 	rm -rf "$xcorr_gen_dir/DATA" &&
 	mkdir "$xcorr_gen_dir/DATA" &&
 	echo $exec_mpi_prep &&
 	eval $exec_mpi_prep > stdout_mpi_prep 2> stderr_mpi_prep &&
 	mpi_time_prep=`cat $fn_t_mpi_prep | tr -d '\n'` &&
-	echo $wf_mpi_prep $platform $version $run_id mpi $number_of_iteration $np_mpi_prep $max_number_of_sieves $max_prime $mpi_time_prep | tee -a "$overall_file" &&
+	echo $wf_mpi_prep $platform $version $run_id mpi $days $trs $np_mpi_prep $mpi_time_prep | tee -a "$overall_file" &&
 
 	rm -rf "$xcorr_gen_dir/XCORR" &&
 	mkdir "$xcorr_gen_dir/XCORR" &&
 	echo $exec_mpi_xcorr &&
 	eval $exec_mpi_xcorr > stdout_mpi_xcorr 2> stderr_mpi_xcorr &&
 	mpi_time_xcorr=`cat $fn_t_mpi_xcorr | tr -d '\n'` &&
-	echo $wf_mpi_xcorr $platform $version $run_id mpi $number_of_iteration $np_mpi_xcorr $max_number_of_sieves $max_prime $mpi_time_xcorr | tee -a "$overall_file"
+	echo $wf_mpi_xcorr $platform $version $run_id mpi $days $trs $np_mpi_xcorr $mpi_time_xcorr | tee -a "$overall_file"
 }
 
 all=(
